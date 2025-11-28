028d1cab567c315e39782ba23998fecb90a19e46
Daksh Balram <dakshabalram@gmail.com>
2025-11-28 08:53:15 -0500

    added tags class (enum)

---
+++ Tags.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Tags - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Tags
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
