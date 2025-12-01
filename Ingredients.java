fff29efc3c1d2a3c6a03e6fda6388f7016f5586e
Daksh Balram <dakhabalram@gmail.com>
2025-12-01 15:00:04 -0500

    Added ingredients abd unit

---
+++ Ingredients.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+
+/**
+ * Write a description of class Ingredients here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Ingredients
+{
+    // instance variables - replace the example below with your own
+    private double amount;
+    private String name;
+    private UnitOfMeasurement unit;
+    private HashSet<String>ingredients;
+
+    /**
+     * Constructor for objects of class Ingredients
+     */
+    public Ingredients()
+    {
+        // initialise instance variables
+        
+    }
+
+}
---
+++ UnitOfMeasurement.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class UnitOfMeasurement - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum UnitOfMeasurement
+{
+    TSP, TBSP, CUP, GALLON, MILLIMETER, LITER, OUNCE, POUND, GRAM, KILLOFGRAM
+}
