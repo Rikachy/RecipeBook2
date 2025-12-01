aae2cd360e85847a70f76001f562dcd3aa4fd1fb
RIchard <richardlife101@gmail.com>
2025-12-01 15:43:52 -0500

    drinks pseudo-code

--- Drinks.java
+++ Drinks.java
@@ -8,7 +8,7 @@
 public class Drinks extends Recipe
 {
     // instance variables - replace the example below with your own
-    private boolean temperature;
+    private boolean isHot;
     private String sizeName;
     private boolean isAlcholholic;
     private boolean hasDairy;
@@ -20,16 +20,33 @@ public class Drinks extends Recipe
         
     }
 
-
+    
     public boolean containsAlchohol()
     {
-        
+        //get ingrdients from Recipe.class for the recipe of called drink
+        //while loop to check if alchohol is in the ingredients list of the called drink
+        // return false if doesn't contain
+        // else return true 
     }
     
+   
     public boolean containsdairy()
     {
-        
+        /*get ingrdients from Recipe.class for the recipe of called drink
+         while loop to check if dairy is in the ingredients list of the called drink
+         the called drink
+         return false if doesn't contain
+         else return true
+         */
     }
     
+    public boolean hotOrCold(boolean temperature){
+        /*
+        gets isHot field
+        if true (drink is hot) systm. prints("it is a hot drink...")  return true 
+        else (drink is cold) syst. prints("it is a cold drink...") return false 
+        */
+      
+    }
     
 }
